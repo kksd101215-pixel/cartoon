1b63cbe09b363a64c3141474ad2f4c3b33f9ef36
kksd101215-pixel <kksd101215@gmail.com>
2026-02-27 22:50:47 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload=function(){
+    document.querySelector(".main-title").style.color="yellow";
+    setTimeout(()=>{
+        document.querySelector(".main-title").style.color="#ff0066";
+    },1000);
+}
